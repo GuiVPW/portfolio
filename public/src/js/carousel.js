95d53a7a3048986bf8359b986a3c15a72b0cca59
Xubilauu <guivpw68@gmail.com>
2020-02-21 20:48:25 -0300

    inicio

--- public/src/js/carousel.js
+++ public/src/js/carousel.js
@@ -1,40 +1,30 @@
-$(".owl-carousel").owlCarousel({
-  items: 4,
-  loop: true,
-  margin: 10,
-  autoplay: true,
-  nav: true,
-  dots: true,
-  autoHeight: true,
-  autoplayTimeout: 1500,
-  autoplayHoverPause: true
-    // items: 5,
-    // lazyLoad: false, 
-    // rtl:true,
-    // rewind: true,
-    // autoplayTimeout: 6000,
-    // autoplayHoverPause: true,
-    // margin: 15,
-    // responsiveClass: true,
-    // autoHeight: true,
-    // autoWidth: true,
-    // smartSpeed: 800,
-    // fluidspeed: 800,
-    // responsive: {
-    //   0: {
-    //     items: 5
-    //   },
+$("#carousel").owlCarousel({
+    lazyLoad: false, 
+    rtl:true,
+    rewind: true,
+    autoplayTimeout: 6000,
+    autoplayHoverPause: true,
+    margin: 15,
+    responsiveClass: true,
+    autoHeight: true,
+    autoWidth: true,
+    smartSpeed: 800,
+    fluidspeed: 800,
+    responsive: {
+      0: {
+        items: 5
+      },
   
-    //   600: {
-    //     items: 5
-    //   },
+      600: {
+        items: 5
+      },
   
-    //   1024: {
-    //     items: 5
-    //   },
+      1024: {
+        items: 5
+      },
   
-    //   1366: {
-    //     items: 5
-    //   }
-    // }
+      1366: {
+        items: 5
+      }
+    }
   });
