040d15869573ac35e565ddb98f7c63d7ae9062de
Xubilauu <guivpw68@gmail.com>
2020-02-21 20:37:26 -0300

    greg

--- public/src/js/carousel.js
+++ public/src/js/carousel.js
@@ -1,31 +1,39 @@
 $(".owl-carousel").owlCarousel({
-    items: 5,
-    lazyLoad: false, 
-    rtl:true,
-    rewind: true,
-    autoplayTimeout: 6000,
-    autoplayHoverPause: true,
-    margin: 15,
-    responsiveClass: true,
-    autoHeight: true,
-    autoWidth: true,
-    smartSpeed: 800,
-    fluidspeed: 800,
-    responsive: {
-      0: {
-        items: 5
-      },
+  items: 4,
+  loop: true,
+  margin: 10,
+  autoplay: true,
+  nav: true,
+  dots: true,
+  autoplayTimeout: 1500,
+  autoplayHoverPause: true
+    // items: 5,
+    // lazyLoad: false, 
+    // rtl:true,
+    // rewind: true,
+    // autoplayTimeout: 6000,
+    // autoplayHoverPause: true,
+    // margin: 15,
+    // responsiveClass: true,
+    // autoHeight: true,
+    // autoWidth: true,
+    // smartSpeed: 800,
+    // fluidspeed: 800,
+    // responsive: {
+    //   0: {
+    //     items: 5
+    //   },
   
-      600: {
-        items: 5
-      },
+    //   600: {
+    //     items: 5
+    //   },
   
-      1024: {
-        items: 5
-      },
+    //   1024: {
+    //     items: 5
+    //   },
   
-      1366: {
-        items: 5
-      }
-    }
+    //   1366: {
+    //     items: 5
+    //   }
+    // }
   });
