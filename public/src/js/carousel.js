de333ef14b518e718c21832c6c13932c1ead0996
Xubilauu <guivpw68@gmail.com>
2020-02-21 22:43:20 -0300

    responsive

--- public/src/js/carousel.js
+++ public/src/js/carousel.js
@@ -5,26 +5,8 @@ $("#carousel").owlCarousel({
     autoplayTimeout: 6000,
     autoplayHoverPause: true,
     margin: 10,
-    responsiveClass: true,
     autoHeight: true,
     autoWidth: true,
     smartSpeed: 800,
-    fluidspeed: 800,
-    responsive: {
-      0: {
-        items: 3
-      },
-  
-      600: {
-        items: 2
-      },
-  
-      1024: {
-        items: 3
-      },
-  
-      1366: {
-        items: 5
-      }
-    }
+    fluidspeed: 800
   });
